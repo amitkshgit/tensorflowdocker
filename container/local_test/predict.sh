b035c8a083b943c72e6356e421237babaa160a8f
System Administrator <root@f45c89a68fa3.ant.amazon.com>
2018-03-20 02:16:04 -0700

    modified predict.sh

--- container/local_test/predict.sh
+++ container/local_test/predict.sh
@@ -3,4 +3,7 @@
 payload=$1
 content=${2:-text/csv}
 
-curl --data-binary @${payload} -H "Content-Type: ${content}" -v http://localhost:8080/invocations
+#curl --data-binary @${payload} -H "Content-Type: text/plain" -v http://localhost:8080/invocations
+curl -d $payload -H "Content-Type: text/plain" -v http://localhost:8080/invocations
+
+#curl --data "url=1905"  http://localhost:8080/invocations
