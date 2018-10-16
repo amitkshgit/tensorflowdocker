12110f5cf5e7f3331c06ff328190ede63cec5e7c
Saida Chanda <viveksaichanda@gmail.com>
2018-10-16 16:46:47 -0700

    Update train_local.sh
    Adding steps to run TensorBoard

--- container/local_test/train_local.sh
+++ container/local_test/train_local.sh
@@ -5,7 +5,15 @@ image=$1
 mkdir -p test_dir/model
 mkdir -p test_dir/output
 
+# Install tensorflow and tensorboard not note book instance to run TensorBoard
+pip install tensorboard
+pip install tensorflow
+
+#create a directory to expose the container logs on to notebook instance
+
+mkdir -p /tmp/retrain_logs
+
 rm test_dir/model/*
 rm test_dir/output/*
 
-docker run -v $(pwd)/test_dir:/opt/ml --rm ${image} train
+docker run -v $(pwd)/test_dir:/opt/ml -v  /tmp/retrain_logs:/tmp/retrain_logs --rm ${image} train
